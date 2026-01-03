b4a4f35b1b406c27242461859a3cee3e69d8c5b6
praveen-7050 <praveen.n7050@gmail.com>
2026-01-03 08:21:46 +0530

    Adding modified list file

--- list.py
+++ list.py
@@ -11,3 +11,49 @@ print(playlist.count("na ready"))
 print(playlist[0:2])
 print(playlist[2:])
 print(playlist[:2])
+print(playlist[-2:])
+
+# list itreation 
+locations= ["Tiruppur","coimbatore","chennai","erode","salem"]
+
+for i in locations:
+    print(i)
+#checking
+if "Coimbatore" in locations:
+    print("yes")
+else :
+    print("No")
+
+#updating the location mutable 
+
+locations[1]='Trichy'
+print(locations)
+#merging
+special = ["clothes","temple","captial","Bedsheet","manogo"]
+
+mixed = locations+special;
+print(mixed)
+
+for i,location in enumerate(mixed):
+    print(f"{i } {location}")
+
+
+a  = [1,2,3,4,5]
+b= a[:]
+print(b)
+c = a.copy()
+print(c)
+a.extend([6,7])
+a.pop()
+del a [2]
+# a.clear()
+ab=a.count(5)
+print(ab)
+# print (a is b)
+# print (a is c)
+print(a)
+
+list = [["ubar","airport", 4.5 ,"coimabtore"]]
+for travller ,pickup,time,drop in list: 
+    print(f"traveller {travller} pickup locaton is {pickup} timing is {time} drop location is {drop} ")
+   
