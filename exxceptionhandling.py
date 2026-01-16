82c2b4865ff0760d2ed4cb5b6d7b558630a1052f
praveen-7050 <praveen.n7050@gmail.com>
2026-01-16 20:22:24 +0530

    Adding the learning exception handling

---
+++ exxceptionhandling.py
@@ -0,0 +1,51 @@
+# try:
+#     print(10/0)
+# except :
+#     print("Cannot divide by Zero")
+
+
+# try :
+#     age = int(input("Enter your age : "))
+#     if(age>=18):
+#         print("your are  elgible to vote")
+#     elif(age<18):
+#         print("your are not elgible to vote")
+# except ValueError :
+#     print(f"the value must be error,{ValueError}")
+# finally :
+#     print("runs the code")
+
+# print("hello world")
+
+# try:
+#     x =10 
+#     x.append(5)
+# except AttributeError as a :
+#     print(f"worng attribute : {a}")
+# print("wdbqwdbq")
+
+
+# try:
+#     marks = float(input("Enter your marks : "))
+#     if(marks <=0):
+#         print(Exception)
+#     elif(marks >=50):
+#         print("You're pass")
+#     elif(marks <50):
+#         print("you're Fail")
+# except Exception as e:
+#     print(f"value_error value must be in float {e}")
+# finally:
+#     print('student_marks_calculation')
+# print("exception handling excetued succesfully ! ")
+
+
+# try:
+#     tup = (10,20,30)
+#     print(f"this is tup {tup}")
+#     tup[2]=(20)
+#     print(f"this is modified tup {tup}")
+# except TypeError as e:
+#     print(f"you cannot add or modified the element in tuple - {e}")
+
+# print("exception handling excetued succesfully ! ")
